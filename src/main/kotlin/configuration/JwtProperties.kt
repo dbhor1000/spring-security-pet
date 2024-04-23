c571b9d98e7c6dc5fdaff339f962fdf29e07dcba
Dmitry Khoroshunov <dbkhoroshunov@mtsbank.ru>
2024-04-23 16:16:54 +0300

    Can add started adding jwt

---
+++ src/main/kotlin/configuration/JwtProperties.kt
@@ -0,0 +1,11 @@
+package org.example.configuration
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "jwt")
+data class JwtProperties (
+
+        val key: String,
+        val accessTokenExpiration: Long,
+        val refreshTokenExpiration: Long,
+)
