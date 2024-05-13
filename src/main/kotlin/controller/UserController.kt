44528d4a88135aabd3a89d05f914635abe1620cd
Dmitry Khoroshunov <dbkhoroshunov@mtsbank.ru>
2024-05-13 15:00:25 +0300

    Working project! Yay! +Refresh Token +Ktlint/readme/controller annotation fix + added an endpoint for sending emails // minor annotation fix

--- src/main/kotlin/controller/UserController.kt
+++ src/main/kotlin/controller/UserController.kt
@@ -6,6 +6,7 @@ import org.example.service.UserService
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @Controller
+@CrossOrigin
 @RequestMapping("/user")
 class UserController(
     private val userService: UserService,
--- src/main/kotlin/entity/User.kt
+++ src/main/kotlin/entity/User.kt
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import org.springframework.web.bind.annotation.CrossOrigin
 
 @Entity
 @Table(name = "users")
